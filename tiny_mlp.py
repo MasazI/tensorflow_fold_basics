5d656953590ddf76225865fa92156778dbb87caf
masazdream <masaz.dream@gmail.com>
2017-02-15 13:48:06 +0900

    initial

---
+++ tiny_mlp.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from six.moves import xrange  # pylint: disable=redefined-builtin
+from six.moves import zip  # pylint: disable=redefined-builtin
+import tensorflow as tf
+import tensorflow_fold.public.blocks as td
